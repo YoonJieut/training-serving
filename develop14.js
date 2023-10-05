106117596cd34bab5f75863c4e455a2b16eb2c81
YoonJieut <qmdldos@gmail.com>
2023-10-06 02:27:30 +0900

    develop14 작성 및 테스트

---
+++ develop14.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const server = http.createServer(function(request, response){
+  console.log(request.url);
+
+  function docMaker(title, href, text) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${title}</title>
+    </head>
+    <body>
+      <a href="${href}">${text}</a>
+    </body>
+    </html>
+    `;
+  }
+  if (request.url === "/") {
+    let doc = docMaker("메인 페이지", "/sub","서브 페이지로 이동");
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.end(doc);
+  }
+  if (request.url === "/sub") {
+    let doc = docMaker("서브 페이지", "/","메인 페이지로 이동");
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.end(doc);
+  }
+});
+server.listen(1111);
